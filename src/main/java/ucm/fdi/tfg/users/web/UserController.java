4f67429c1bb4d6ae71706ce0e1877a5f2cec6642
David Rico Valverde <davidrico77@gmail.com>
2016-01-19 20:52:27 +0100

    Añadido constructor de UserController

--- src/main/java/ucm/fdi/tfg/users/web/UserController.java
+++ src/main/java/ucm/fdi/tfg/users/web/UserController.java
@@ -1,9 +1,20 @@
 package ucm.fdi.tfg.users.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+
+import ucm.fdi.tfg.users.business.boundary.UserManager;
 
 @Controller
 public class UserController {
+	
+	private UserManager userManager;
+	
+	@Autowired
+	public UserController(UserManager userManager)
+	{
+		this.userManager = userManager;
+	}
 	
 	
 	
