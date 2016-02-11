f20ce8fa72864936e4817b93e458b19e941c070a
David Nieto <david.nietocobo@gmail.com>
2016-02-11 14:22:04 +0100

    Arreglado el método findByUsername del repositorio, que devuelve un usuario pasandole su username.

--- src/main/java/ucm/fdi/tfg/users/business/repository/UserRepository.java
+++ src/main/java/ucm/fdi/tfg/users/business/repository/UserRepository.java
@@ -1,7 +1,5 @@
 package ucm.fdi.tfg.users.business.repository;
 
-import java.util.ArrayList;
-
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
@@ -13,23 +11,9 @@ import ucm.fdi.tfg.users.business.entity.UserRole;
 
 @Repository
 public class UserRepository {
-	
-	
-	 ArrayList<User> userList;
 	 
 	@PersistenceContext
-	private EntityManager em;
-	 
-	
-	/*public UserRepository(){
-		userList = new ArrayList<User>();
-		User u = new User("david","fdi");
-		u.addRole(new UserRole("ROLE_ADMIN"));
-		u.addRole(new UserRole("ROLE_USER"));
-		userList.add(u);
-		
-		em.merge(u);
-	}*/
+	private EntityManager em; 
 	
 	public void save(User user){
 		user.addRole(new UserRole("ROLE_USER"));
@@ -38,23 +22,21 @@ public class UserRepository {
 		em.merge(user);
 	}
 	
+
 	public User findByUsername(String username){
-		/*for(User u : userList){
-			if (u.getName().equals(username))
-				return u;
-		}
-		return null */
 		
-		//No funciona con find
-		//User u = em.find(User.class, username);
-			User us = new User("hola","adios");
-		  Query query = em.createQuery("SELECT u FROM User u where u.username=" +username);
-		  em.persist(us);
-		  User u =  (User) query.getSingleResult();
+		  Query query = em.createQuery("SELECT u FROM User u where u.username= :username");
+		  query.setParameter("username",username);
+		  
+		  //Como solo quiero un objeto, uso getsingleresult
+		  User u = (User) query.getSingleResult();
+		
+		 
+		 // Find solo funciona pasandole un entero (por id)
+		 // User u = em.find(User.class, 1);
+		  
 		  return u;
-		
-		
-		
+	
 	}
 	
 
