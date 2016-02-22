f37a93aaf1709e1eda69f82d6cffc32895c46fce
David Rico <davidrico77@gmail.com>
2016-02-22 19:05:14 +0100

    Añadido servicio de aplicacion PagoManager

---
+++ src/main/java/ucm/fdi/tfg/pagos/business/boundary/PagoManager.java
@@ -0,0 +1,28 @@
+package ucm.fdi.tfg.pagos.business.boundary;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import ucm.fdi.tfg.pagos.business.entity.Pago;
+import ucm.fdi.tfg.pagos.business.repository.PagoRepository;
+import ucm.fdi.tfg.users.business.entity.User;
+import ucm.fdi.tfg.users.business.entity.UserRole;
+
+@Service
+@Transactional
+public class PagoManager {
+	
+	PagoRepository repositoryPago;
+	
+	 @Autowired
+	 public PagoManager (PagoRepository pagos){
+		 this.repositoryPago = pagos;
+	 }
+	 
+	 public void save(Pago pago){	
+		repositoryPago.save(pago);
+	}
+	
+}
