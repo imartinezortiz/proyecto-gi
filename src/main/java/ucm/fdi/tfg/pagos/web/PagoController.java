7b2a85ba4c168feaabfd5687c195bb68c65fd900
David Nieto <david.nietocobo@gmail.com>
2016-02-25 18:08:42 +0100

    Funcionando el formulario Pago con los gastos dinamicos

--- src/main/java/ucm/fdi/tfg/pagos/web/PagoController.java
+++ src/main/java/ucm/fdi/tfg/pagos/web/PagoController.java
@@ -1,15 +1,19 @@
 package ucm.fdi.tfg.pagos.web;
 
+import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import ucm.fdi.tfg.pagos.business.boundary.PagoManager;
+import ucm.fdi.tfg.pagos.business.entity.Gasto;
 import ucm.fdi.tfg.pagos.business.entity.Pago;
 
 @Controller
@@ -30,7 +34,9 @@ public class PagoController {
 		Map<String, Object> model = new HashMap<String, Object>();
 		
 		model.put("usuario", null);
-		
+		Pago pago = new Pago();
+		pago.getGastos().add(new Gasto("0001", "Hola", BigDecimal.ONE));
+		model.put("pago", pago);
 		ModelAndView view = new ModelAndView("PagoCabecera", model);
 		
 		return view;
@@ -39,7 +45,7 @@ public class PagoController {
 	
 	//Por aqui entra la aplicacion
 	@RequestMapping(value = "/addPago", method = RequestMethod.POST)
-	public String addPago(Pago pago){
+	public String addPago(@ModelAttribute Pago pago, BindingResult errors){
 		System.out.println("AÑADIENDO PAGO");
 		try{ 
 		 pagoManager.save(pago);		 
