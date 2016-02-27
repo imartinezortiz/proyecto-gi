4c08170814fc5a89e1102657ddbca3b2490fb7b6
David Rico Valverde <davidrico77@gmail.com>
2016-02-27 22:14:35 +0100

    Corregido conflicto en PagoController

--- src/main/java/ucm/fdi/tfg/pagos/web/PagoController.java
+++ src/main/java/ucm/fdi/tfg/pagos/web/PagoController.java
@@ -1,13 +1,17 @@
 package ucm.fdi.tfg.pagos.web;
 
 import java.math.BigDecimal;
+
 import java.util.ArrayList;
 import java.util.Collection;
+
 import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -33,30 +37,27 @@ public class PagoController {
 		
 		Map<String, Object> model = new HashMap<String, Object>();
 		
-		Gasto gasto = new Gasto ("numFactura","provedor",new BigDecimal(2.3));		
-				
-		Collection<Gasto> gastos = new ArrayList<Gasto>();
-		
-        gastos.add(gasto);
-       
-		model.put("gastos", gastos);
-		
+		Pago pago = new Pago();
+		pago.getGastos().add(new Gasto("0001", "Hola", BigDecimal.ONE));
+		model.put("pago", pago);
+
 		ModelAndView view = new ModelAndView("PagoCabecera", model);
 		
 		return view;
 	}
 	
 	
-	//Por aqui entra la aplicacion
+	//Por aqui entra la aplicacion.
+	//Igual que en thymeleaf le decimos mediante th:object el objeto q mandamos,  
+	//en spring mediante ModelAttribute le decimos el tipo de objeto que le llega.
 	@RequestMapping(value = "/addPago", method = RequestMethod.POST)
-	public String addPago(Pago pago){
+	public String addPago(@ModelAttribute Pago pago, BindingResult errors){
 		System.out.println("AÑADIENDO PAGO");
 		try{ 
 		 pagoManager.save(pago);		 
 		}catch(Exception e){
 			 return "redirect:/error";
 		 }
-		
 		return "redirect:/registroCompleto";
 	}
 	
