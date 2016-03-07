400ba077df49abc7155c3d386d701b423fbb5eef
David Nieto <david.nietocobo@gmail.com>
2016-03-07 20:51:36 +0100

    Añadido constructor por defecto, relacion ManyToOne y getters(setter de proyecto

--- src/main/java/ucm/fdi/tfg/proyecto/business/entity/Proyecto.java
+++ src/main/java/ucm/fdi/tfg/proyecto/business/entity/Proyecto.java
@@ -1,10 +1,11 @@
 package ucm.fdi.tfg.proyecto.business.entity;
 
-import javax.persistence.Column;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 
 import ucm.fdi.tfg.investigadores.business.entity.Investigador;
 
@@ -16,7 +17,45 @@ public class Proyecto {
 	Long id;
 	String referencia;
 	String titulo;
+	@ManyToOne
 	Investigador investigador;
 	String numContabilidad;
+	
+	public Proyecto(){
+	
+	}
+
+	public String getReferencia() {
+		return referencia;
+	}
+
+	public void setReferencia(String referencia) {
+		this.referencia = referencia;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public Investigador getInvestigador() {
+		return investigador;
+	}
+
+	public void setInvestigador(Investigador investigador) {
+		this.investigador = investigador;
+	}
+
+	public String getNumContabilidad() {
+		return numContabilidad;
+	}
+
+	public void setNumContabilidad(String numContabilidad) {
+		this.numContabilidad = numContabilidad;
+	}
+	
 
 }
