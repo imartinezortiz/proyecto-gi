4c02a6caaec26681ca87ef5c09bca625690a5eac
David Rico Valverde <davidrico77@gmail.com>
2016-05-06 20:23:07 +0200

    Arreglado SELECT DISTINCT

--- src/main/java/ucm/fdi/tfg/pagos/business/control/PagosRepository.java
+++ src/main/java/ucm/fdi/tfg/pagos/business/control/PagosRepository.java
@@ -13,7 +13,8 @@ import ucm.fdi.tfg.pagos.business.entity.Pago;
 @Repository
 public interface PagosRepository extends JpaRepository<Pago, Long> {	
 		
-	@Query(" SELECT I FROM Pago I  LEFT JOIN I.gastos G JOIN FETCH I.proyecto P WHERE I.proyecto.id = :idProyecto")
+	//@Query(" SELECT DISTINCT I FROM Pago I LEFT JOIN  I.gastos G JOIN FETCH I.proyecto P WHERE I.proyecto.id = :idProyecto")
+	@Query(" SELECT I FROM Pago I WHERE I.proyecto.id = :idProyecto")
 	public List<Pago> pagosPorProyecto(@Param(value = "idProyecto") Long idProyecto);
 
 
