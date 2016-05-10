05882e8f2ce1dfa30e0a081fb504ef59301d5102
David Nieto <david.nietocobo@gmail.com>
2016-05-10 21:25:17 +0200

    Funcionando Eliminar. Cambiadas las URLs

--- src/main/java/ucm/fdi/tfg/fileupload/web/FileUploadController.java
+++ src/main/java/ucm/fdi/tfg/fileupload/web/FileUploadController.java
@@ -1,6 +1,7 @@
 package ucm.fdi.tfg.fileupload.web;
 
 import java.io.IOException;
+import java.util.Iterator;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -14,6 +15,7 @@ import org.springframework.web.servlet.ModelAndView;
 
 import ucm.fdi.tfg.fileupload.business.boundary.AttachmentManager;
 import ucm.fdi.tfg.fileupload.business.boundary.NewFileCommand;
+import ucm.fdi.tfg.fileupload.business.entity.Attachment;
 import ucm.fdi.tfg.inventarios.business.boundary.InventariosManager;
 import ucm.fdi.tfg.inventarios.business.entity.Inventario;
 
@@ -29,54 +31,37 @@ public class FileUploadController {
 		this.manager = manager;
 		this.inventarioManager = inventarioManager;
 	}
-
-	@RequestMapping(method = RequestMethod.GET, value = "/files")
-	public ModelAndView files() {
-		ModelAndView view = new ModelAndView("files");
-		System.out.println("entra x el controlador");
-		view.addObject("files", manager.getAttachments());
-		view.addObject("command", new NewFileCommand());
-		return view;
-	}
-
-	@RequestMapping(method = RequestMethod.POST, value = "/files")
-	public ModelAndView addAttachment(@ModelAttribute("command") @Validated NewFileCommand command,
-			BindingResult result) throws IOException {
+	
+	@RequestMapping(method = RequestMethod.DELETE, value = "proyectos/{idProyecto}/inventario/{idInventario}/adjunto/{id}")
+	public ModelAndView deleteAttachment(@PathVariable("id") long id ,  @PathVariable("idInventario") long idInventario, @PathVariable("idProyecto") long idProyecto) {
 		
-
-		if (result.hasErrors()) {
-			ModelAndView view = new ModelAndView("files");
-			view.addObject("files", manager.getAttachments());
-			view.addObject("command", command);
-			return view;
+		Inventario inventario = inventarioManager.findOneInventario(idInventario);
+		
+		Iterator<Attachment> it = inventario.getAdjuntos().iterator();
+		while(it.hasNext()){
+			if (it.next().getId() == id)
+				it.remove();
 		}
-
-		manager.addAttachment(command);
-
-		return new ModelAndView("redirect:/files");
-	}
-	
-	@RequestMapping(method = RequestMethod.DELETE, value = "/files/{id}")
-	public ModelAndView deleteAttachment(@PathVariable("id") long id) {
+		
+		inventarioManager.save(inventario);
 		manager.deleteAttachment(id);
-		return new ModelAndView("redirect:/files");
+		System.out.println(inventario.getAdjuntos());
+		return new ModelAndView("redirect:/proyectos/"+idProyecto+"/inventario/"+idInventario+"/adjuntos");
 	}
+
 	
-	@RequestMapping(method = RequestMethod.GET, value = "proyectos/{idProyecto}/{idInventario}/adjuntos")
+	@RequestMapping(method = RequestMethod.GET, value = "proyectos/{idProyecto}/inventario/{idInventario}/adjuntos")
 	public ModelAndView filesPrueba(@PathVariable("idInventario") long idInventario) {
 		ModelAndView view = new ModelAndView("files");
-		System.out.println("entra x el controlador");
-		//view.addObject("files", manager.getAttachments());
 		view.addObject("files", inventarioManager.findOneInventario(idInventario).getAdjuntos());
 		view.addObject("command", new NewFileCommand());
 		return view;
 	}
 
-	@RequestMapping(method = RequestMethod.POST, value = "proyectos/{idProyecto}/{idInventario}/adjuntos")
+	@RequestMapping(method = RequestMethod.POST, value = "proyectos/{idProyecto}/inventario/{idInventario}/adjuntos")
 	public ModelAndView addAttachmentPrueba(@PathVariable("idInventario") long idInventario ,@PathVariable("idProyecto") long idProyecto, @ModelAttribute("command") @Validated NewFileCommand command,
 			BindingResult result) throws IOException {
 		
-
 		if (result.hasErrors()) {
 			ModelAndView view = new ModelAndView("files");
 			view.addObject("files", manager.getAttachments());
@@ -84,11 +69,10 @@ public class FileUploadController {
 			return view;
 		}
 
-		
 		Inventario inv = inventarioManager.findOneInventario(idInventario);
 		inv.getAdjuntos().add(manager.addAttachment(command));
 		inventarioManager.save(inv);
-		return new ModelAndView("redirect:/proyectos/"+idProyecto+"/"+idInventario+"/adjuntos");
+		return new ModelAndView("redirect:/proyectos/"+idProyecto+"/inventario/"+idInventario+"/adjuntos");
 	}
 	
 	
