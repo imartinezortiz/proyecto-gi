748d8fc436e58bb70ce22164b4176a847e6df8e3
David Nieto <david.nietocobo@gmail.com>
2016-05-10 21:25:42 +0200

    Cambiado a OneToMany, para poder darle el cascadeType

--- src/main/java/ucm/fdi/tfg/inventarios/business/entity/Inventario.java
+++ src/main/java/ucm/fdi/tfg/inventarios/business/entity/Inventario.java
@@ -4,12 +4,13 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
-import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
@@ -50,7 +51,7 @@ public class Inventario {
 	@NotNull(message="Introduce una fecha")
 	private LocalDate fecha;
 	
-	@ElementCollection
+	@OneToMany(cascade = CascadeType.REMOVE)
 	private List<Attachment> adjuntos;
 	
 	public List<Attachment> getAdjuntos() {
