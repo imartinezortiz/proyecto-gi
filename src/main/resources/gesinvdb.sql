d0186b52838e49071b25c0089910bc9a40875fd9
David Rico Valverde <davidrico77@gmail.com>
2016-05-18 20:51:08 +0200

    Mejorando diseño

--- src/main/resources/gesinvdb.sql
+++ src/main/resources/gesinvdb.sql
@@ -1,13 +1,13 @@
 SET character_set_client = utf8;
 
-INSERT INTO users (usersId,accountExpired, accountLocked, apellidos, credentialsExpired,email,enabled, nombre, password, telefono, username) VALUES (1, b'0', b'0', 'rico', b'0', 'drv@gmail.com', b'1', 'david', '$2a$10$//HU8yTgOoLrZX6z9cgXhuR/IJki.BtS/ZM4Pr8s/zBDCFIT.I3P6', '607', 'root');
-INSERT INTO users (usersId,accountExpired, accountLocked, apellidos, credentialsExpired,email,enabled, nombre, password, telefono, username) VALUES (2, b'0', b'0', 'ap2', b'0', 'email2@gmail.com', b'1', 'admin1', '$2a$10$n9v06bDngTF/33M9sPdRTeJQjWwTm4pJWqhbGaMYSDumEnXy.T2.G', 'tel2', 'administrador');
-INSERT INTO users (usersId,accountExpired, accountLocked, apellidos, credentialsExpired,email,enabled, nombre, password, telefono, username) VALUES (3, b'0', b'0', 'apell3', b'0', 'email3@gmail.com', b'1', 'g1', '$2a$10$tfVFydVKjyHlRWJ/aDZKq.t5Hf/xGA.XwrMS5ZGgF5wFHUDwhPdxG', 'telf3', 'gestor');
-INSERT INTO users (usersId,accountExpired, accountLocked, apellidos, credentialsExpired,email,enabled, nombre, password, telefono, username) VALUES (4, b'0', b'0', 'Nieto', b'0', 'emai1@gmail.com', b'1', 'David', '$2a$10$aY8xNKxmWpuU7Iu632pYpeuYJYOSYYzOvGBEkAuqAqp1XtA0afuJW', 'tel1', 'invDavid');
-INSERT INTO users (usersId,accountExpired, accountLocked, apellidos, credentialsExpired,email,enabled, nombre, password, telefono, username) VALUES (5, b'0', b'0', 'Rico', b'0', '5@gmail.com', b'1', 'David', '$2a$10$aY8xNKxmWpuU7Iu632pYpeuYJYOSYYzOvGBEkAuqAqp1XtA0afuJW', 'tel5', 'invIvan');
-INSERT INTO users (usersId,accountExpired, accountLocked, apellidos, credentialsExpired,email,enabled, nombre, password, telefono, username) VALUES (6, b'0', b'0', 'Martinez', b'0', '6@gmail.com', b'1', 'Ivan', '$2a$10$aY8xNKxmWpuU7Iu632pYpeuYJYOSYYzOvGBEkAuqAqp1XtA0afuJW', 'tel6', 'invNieto');
-INSERT INTO users (usersId,accountExpired, accountLocked, apellidos, credentialsExpired,email,enabled, nombre, password, telefono, username) VALUES (7, b'0', b'0', 'Torres', b'0', '7@gmail.com', b'1', 'Fernando', '$2a$10$aY8xNKxmWpuU7Iu632pYpeuYJYOSYYzOvGBEkAuqAqp1XtA0afuJW', 'tel7', 'invRico');
-INSERT INTO users (usersId,accountExpired, accountLocked, apellidos, credentialsExpired,email,enabled, nombre, password, telefono, username) VALUES (8, b'0', b'0', 'Rebollo', b'0', '8@gmail.com', b'1', 'Marcos', '$2a$10$aY8xNKxmWpuU7Iu632pYpeuYJYOSYYzOvGBEkAuqAqp1XtA0afuJW', 'tel8', 'invMarcos');
+INSERT INTO users (usersId,accountExpired, accountLocked, apellidos, credentialsExpired,email,enabled, nombre, password, telefono, username) VALUES (1, b'0', b'0', 'Rico Valverde', b'0', 'drv@gmail.com', b'1', 'David', '$2a$10$//HU8yTgOoLrZX6z9cgXhuR/IJki.BtS/ZM4Pr8s/zBDCFIT.I3P6', '607606620', 'root');
+INSERT INTO users (usersId,accountExpired, accountLocked, apellidos, credentialsExpired,email,enabled, nombre, password, telefono, username) VALUES (2, b'0', b'0', 'Nieto Cobo', b'0', 'email2@gmail.com', b'1', 'David', '$2a$10$n9v06bDngTF/33M9sPdRTeJQjWwTm4pJWqhbGaMYSDumEnXy.T2.G', '612357854', 'administrador');
+INSERT INTO users (usersId,accountExpired, accountLocked, apellidos, credentialsExpired,email,enabled, nombre, password, telefono, username) VALUES (3, b'0', b'0', 'Sánchez Torquemada', b'0', 'email3@gmail.com', b'1', 'Julio', '$2a$10$tfVFydVKjyHlRWJ/aDZKq.t5Hf/xGA.XwrMS5ZGgF5wFHUDwhPdxG', '683123654', 'gestor');
+INSERT INTO users (usersId,accountExpired, accountLocked, apellidos, credentialsExpired,email,enabled, nombre, password, telefono, username) VALUES (4, b'0', b'0', 'Roca Martínez', b'0', 'emai1@gmail.com', b'1', 'Miguel', '$2a$10$aY8xNKxmWpuU7Iu632pYpeuYJYOSYYzOvGBEkAuqAqp1XtA0afuJW', '612509750', 'invDavid');
+INSERT INTO users (usersId,accountExpired, accountLocked, apellidos, credentialsExpired,email,enabled, nombre, password, telefono, username) VALUES (5, b'0', b'0', 'Ramos Atienza', b'0', '5@gmail.com', b'1', 'Pablo', '$2a$10$aY8xNKxmWpuU7Iu632pYpeuYJYOSYYzOvGBEkAuqAqp1XtA0afuJW', '683511224', 'invIvan');
+INSERT INTO users (usersId,accountExpired, accountLocked, apellidos, credentialsExpired,email,enabled, nombre, password, telefono, username) VALUES (6, b'0', b'0', 'Gonzalez Prieto', b'0', '6@gmail.com', b'1', 'Ivan', '$2a$10$aY8xNKxmWpuU7Iu632pYpeuYJYOSYYzOvGBEkAuqAqp1XtA0afuJW', '654199009', 'invNieto');
+INSERT INTO users (usersId,accountExpired, accountLocked, apellidos, credentialsExpired,email,enabled, nombre, password, telefono, username) VALUES (7, b'0', b'0', 'Garía Romero', b'0', '7@gmail.com', b'1', 'Fernando', '$2a$10$aY8xNKxmWpuU7Iu632pYpeuYJYOSYYzOvGBEkAuqAqp1XtA0afuJW', '688332156', 'invRico');
+INSERT INTO users (usersId,accountExpired, accountLocked, apellidos, credentialsExpired,email,enabled, nombre, password, telefono, username) VALUES (8, b'0', b'0', 'Rebollo Fernández', b'0', '8@gmail.com', b'1', 'Marcos', '$2a$10$aY8xNKxmWpuU7Iu632pYpeuYJYOSYYzOvGBEkAuqAqp1XtA0afuJW', '650099888', 'invMarcos');
 
 
 INSERT INTO user_roles (usersId, role) VALUES (1, 'ROLE_ADMIN');
@@ -39,19 +39,19 @@ INSERT INTO user_roles (usersId, role) VALUES(7, 'ROLE_USER');
 INSERT INTO user_roles (usersId, role) VALUES(8, 'ROLE_INVESTIGADOR');
 INSERT INTO user_roles (usersId, role) VALUES(8, 'ROLE_USER');
 
-INSERT INTO investigadores (investigadorId,centro, departamento) VALUES (4, 'C1', 'D1');
-INSERT INTO investigadores (investigadorId,centro, departamento) VALUES (5, 'C5', 'D5');
-INSERT INTO investigadores (investigadorId,centro, departamento) VALUES (6, 'C6', 'D6');
-INSERT INTO investigadores (investigadorId,centro, departamento) VALUES (7, 'C7', 'D7');
-INSERT INTO investigadores (investigadorId,centro, departamento) VALUES (8, 'C8', 'D8');
+INSERT INTO investigadores (investigadorId,centro, departamento) VALUES (4, 'Informática', 'Sistemas Informáticos y Computación');
+INSERT INTO investigadores (investigadorId,centro, departamento) VALUES (5, 'Matemáticas', 'Análisis Matemático');
+INSERT INTO investigadores (investigadorId,centro, departamento) VALUES (6, 'Físicas', 'Óptica');
+INSERT INTO investigadores (investigadorId,centro, departamento) VALUES (7, 'Informática', 'Inteligencia Artificial');
+INSERT INTO investigadores (investigadorId,centro, departamento) VALUES (8, 'Estadística', 'Economía Financiera y Contabilidad');
 
 
-INSERT INTO proyectos (id, numContabilidad, referencia, titulo, investigadorID) VALUES (1, '150', 'FA1903', 'Primer Proyecto', 4);
-INSERT INTO proyectos (id, numContabilidad, referencia, titulo, investigadorID) VALUES (2, '200', 'atm1903', 'Segundo Proyecto', 5);
-INSERT INTO proyectos (id, numContabilidad, referencia, titulo, investigadorID) VALUES (3, '250', 'FDI', 'Tercer Proyecto', 6);
-INSERT INTO proyectos (id, numContabilidad, referencia, titulo, investigadorID) VALUES (4, '300', 'UCM', 'Cuarto Proyecto', 7);
-INSERT INTO proyectos (id, numContabilidad, referencia, titulo, investigadorID) VALUES (5, '350', 'Politecnica', 'Quinto Proyecto', 8);
-INSERT INTO proyectos (id, numContabilidad, referencia, titulo, investigadorID) VALUES (6, '400', 'Eptisa', 'Sexto Proyecto', 4);
+INSERT INTO proyectos (id, numContabilidad, referencia, titulo, investigadorID) VALUES (1, '150', 'CM1101', 'Primer Proyecto', 4);
+INSERT INTO proyectos (id, numContabilidad, referencia, titulo, investigadorID) VALUES (2, '200', 'D43', 'Segundo Proyecto', 5);
+INSERT INTO proyectos (id, numContabilidad, referencia, titulo, investigadorID) VALUES (3, '250', 'MAT2010', 'Tercer Proyecto', 6);
+INSERT INTO proyectos (id, numContabilidad, referencia, titulo, investigadorID) VALUES (4, '300', 'MAT2009', 'Cuarto Proyecto', 7);
+INSERT INTO proyectos (id, numContabilidad, referencia, titulo, investigadorID) VALUES (5, '350', 'GR35', 'Quinto Proyecto', 8);
+INSERT INTO proyectos (id, numContabilidad, referencia, titulo, investigadorID) VALUES (6, '400', 'GR58', 'Sexto Proyecto', 4);
 INSERT INTO proyectos (id, numContabilidad, referencia, titulo, investigadorID) VALUES (7, '450', 'IMCs', 'Septimo Proyecto', 5);
 INSERT INTO proyectos (id, numContabilidad, referencia, titulo, investigadorID) VALUES (8, '500', 'Avanade', 'Octavo Proyecto', 6);
 INSERT INTO proyectos (id, numContabilidad, referencia, titulo, investigadorID) VALUES (9, '550', 'GTI', 'Noveno Proyecto', 7);
@@ -64,10 +64,10 @@ INSERT INTO proyectos_investigadores (proyectos_id, investigadores_investigadorI
 INSERT INTO proyectos_investigadores (proyectos_id, investigadores_investigadorId) VALUES (2,6);
 INSERT INTO proyectos_investigadores (proyectos_id, investigadores_investigadorId) VALUES (2,7);
 
-INSERT INTO pagos(pagoId, numOrden,fecha,iban,bic,pagador,memoria,relacion, fase, proyecto_id) VALUES (1,111,'2016-04-21','100','150','David','Memoriaaa','La relacion misma','PROCESANDO',1);
-INSERT INTO pagos(pagoId, numOrden,fecha,iban,bic,pagador,memoria,relacion, fase, proyecto_id) VALUES (2,111,'2016-05-25','200','250','Ivan','Memoriaaa','r2', 'EDICION',1);
+INSERT INTO pagos(pagoId, numOrden,fecha,iban,bic,pagador,memoria,relacion, fase, proyecto_id) VALUES (1,111,'2016-04-21','100','150','David García López','Mem1','REL-1','PROCESANDO',1);
+INSERT INTO pagos(pagoId, numOrden,fecha,iban,bic,pagador,memoria,relacion, fase, proyecto_id) VALUES (2,222,'2016-05-25','200','250','Iván Velasco Mora','Mem2','REL-2', 'EDICION',1);
 
-INSERT INTO pago_gastos (pagoId, importe, numFactura, proveedor) VALUES (1, '100.00', '222', 'p1');
+INSERT INTO pago_gastos (pagoId, importe, numFactura, proveedor) VALUES (1, '100.00', '111', 'p1');
 INSERT INTO pago_gastos (pagoId, importe, numFactura, proveedor) VALUES (2, '200.00', '222', 'p1');
 
 INSERT INTO dietas (pais,importe) VALUES ('España','37.40');
@@ -172,15 +172,15 @@ INSERT INTO dietas (pais,importe) VALUES ('Resto del mundo','40.87');
 
 
 --Insertamos inventarios de prueba para el proyecto 1 (Primer Proyecto);
-INSERT INTO inventarios (inventarioId, autorizacion, centro, descripcion, fecha, observaciones, fase, proyecto_id) VALUES (1, 'autor1', 'c1', 'des1', '2016-02-21', 'obs1', 'EDICION',1);
-INSERT INTO inventarios (inventarioId, autorizacion, centro, descripcion, fecha, observaciones, fase, proyecto_id) VALUES (2, 'autor2', 'c2', 'des2', '2016-03-12', 'obs2', 'EDICION',1);
-INSERT INTO inventarios (inventarioId, autorizacion, centro, descripcion, fecha, observaciones, fase, proyecto_id) VALUES (3, 'autor1', 'c1', 'des1', '2016-04-23', 'obs1', 'EDICION',1);
-INSERT INTO inventarios (inventarioId, autorizacion, centro, descripcion, fecha, observaciones, fase, proyecto_id) VALUES (4, 'autor1', 'c1', 'des1', '2016-06-24', 'obs1', 'PROCESANDO',1);
-INSERT INTO inventarios (inventarioId, autorizacion, centro, descripcion, fecha, observaciones, fase, proyecto_id) VALUES (5, 'autor1', 'c1', 'des1', '2016-07-25', 'obs1', 'PROCESANDO',1);
-INSERT INTO inventarios (inventarioId, autorizacion, centro, descripcion, fecha, observaciones, fase, proyecto_id) VALUES (6, 'autor1', 'c1', 'des1', '2016-08-26', 'obs1', 'PROCESANDO',1);
-INSERT INTO inventarios (inventarioId, autorizacion, centro, descripcion, fecha, observaciones, fase, proyecto_id) VALUES (7, 'autor1', 'c1', 'des1', '2016-09-27', 'obs1', 'PROCESANDO',1);
+INSERT INTO inventarios (inventarioId, autorizacion, centro, descripcion, fecha, observaciones, fase, proyecto_id) VALUES (1, 'autor1', 'Matemáticas', 'des1', '2016-02-21', 'obs1', 'EDICION',1);
+INSERT INTO inventarios (inventarioId, autorizacion, centro, descripcion, fecha, observaciones, fase, proyecto_id) VALUES (2, 'autor2', 'Físicas', 'des2', '2016-03-12', 'obs2', 'EDICION',1);
+INSERT INTO inventarios (inventarioId, autorizacion, centro, descripcion, fecha, observaciones, fase, proyecto_id) VALUES (3, 'autor1', 'Matemáticas', 'des1', '2016-04-23', 'obs3', 'EDICION',1);
+INSERT INTO inventarios (inventarioId, autorizacion, centro, descripcion, fecha, observaciones, fase, proyecto_id) VALUES (4, 'autor1', 'Informática', 'des1', '2016-06-24', 'obs4', 'PROCESANDO',1);
+INSERT INTO inventarios (inventarioId, autorizacion, centro, descripcion, fecha, observaciones, fase, proyecto_id) VALUES (5, 'autor1', 'Matemáticas', 'des1', '2016-07-25', 'obs5', 'PROCESANDO',1);
+INSERT INTO inventarios (inventarioId, autorizacion, centro, descripcion, fecha, observaciones, fase, proyecto_id) VALUES (6, 'autor1', 'Informática', 'des1', '2016-08-26', 'obs6', 'PROCESANDO',1);
+INSERT INTO inventarios (inventarioId, autorizacion, centro, descripcion, fecha, observaciones, fase, proyecto_id) VALUES (7, 'autor1', 'Informática', 'des1', '2016-09-27', 'obs7', 'PROCESANDO',1);
 -- Inventario de proyecto 2
-INSERT INTO inventarios (inventarioId, autorizacion, centro, descripcion, fecha, observaciones, fase, proyecto_id) VALUES (8, 'autor1', 'c1', 'des1', '2016-04-21', 'obs1','EDICION', 2);
+INSERT INTO inventarios (inventarioId, autorizacion, centro, descripcion, fecha, observaciones, fase, proyecto_id) VALUES (8, 'autor1', 'Informática', 'des1', '2016-04-21', 'obs8','EDICION', 2);
 
 
 --Insertamos viajes de prueba para el proyecto 1 (Primer Proyecto);
