0b6b3a951970ed5b03dcf4c808fc3b4f92423e3d
David Nieto <david.nietocobo@gmail.com>
2016-06-11 14:04:39 +0200

    Creado GET para AltaComisionServicio

--- src/main/java/ucm/fdi/tfg/viajes/business/entity/ComisionServicio.java
+++ src/main/java/ucm/fdi/tfg/viajes/business/entity/ComisionServicio.java
@@ -3,6 +3,7 @@ package ucm.fdi.tfg.viajes.business.entity;
 import java.time.LocalDate;
 import java.util.Map;
 
+import javax.persistence.Column;
 import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
@@ -42,6 +43,19 @@ public class ComisionServicio {
 	
 	private TransportesEnum transportePrincipal;
 	
+	
+	@Type(type="org.jadira.usertype.dateandtime.threeten.PersistentLocalDate")
+	@DateTimeFormat(iso = ISO.DATE)
+	@Column(nullable=false)
+	private LocalDate inicio;
+	
+	
+
+	@Type(type="org.jadira.usertype.dateandtime.threeten.PersistentLocalDate")
+	@DateTimeFormat(iso = ISO.DATE)
+	@Column(nullable=false)
+	private LocalDate fin;
+	
 
 	private EstadoComisionServicioEnum estado;
 	
@@ -51,4 +65,107 @@ public class ComisionServicio {
 	@Type(type="org.jadira.usertype.dateandtime.threeten.PersistentLocalDate")
 	@DateTimeFormat(iso = ISO.DATE)
 	private Map<EstadoComisionServicioEnum, LocalDate> vbs;
+	
+	public ComisionServicio(Proyecto proyecto, Investigador interesado) {
+		this.proyecto = proyecto;
+		this.estado = EstadoComisionServicioEnum.EDICION;
+		this.interesado = interesado;
+	
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Proyecto getProyecto() {
+		return proyecto;
+	}
+
+	public void setProyecto(Proyecto proyecto) {
+		this.proyecto = proyecto;
+	}
+
+	public Investigador getInteresado() {
+		return interesado;
+	}
+
+	public void setInteresado(Investigador interesado) {
+		this.interesado = interesado;
+	}
+
+	public String getItinerario() {
+		return itinerario;
+	}
+
+	public void setItinerario(String itinerario) {
+		this.itinerario = itinerario;
+	}
+
+	public String getObjetoDesplazamiento() {
+		return objetoDesplazamiento;
+	}
+
+	public void setObjetoDesplazamiento(String objetoDesplazamiento) {
+		this.objetoDesplazamiento = objetoDesplazamiento;
+	}
+
+	public boolean isGastosInscripcion() {
+		return gastosInscripcion;
+	}
+
+	public void setGastosInscripcion(boolean gastosInscripcion) {
+		this.gastosInscripcion = gastosInscripcion;
+	}
+
+	public String getObservaciones() {
+		return observaciones;
+	}
+
+	public void setObservaciones(String observaciones) {
+		this.observaciones = observaciones;
+	}
+
+	public TransportesEnum getTransportePrincipal() {
+		return transportePrincipal;
+	}
+
+	public void setTransportePrincipal(TransportesEnum transportePrincipal) {
+		this.transportePrincipal = transportePrincipal;
+	}
+
+	public EstadoComisionServicioEnum getEstado() {
+		return estado;
+	}
+
+	public void setEstado(EstadoComisionServicioEnum estado) {
+		this.estado = estado;
+	}
+
+	public Map<EstadoComisionServicioEnum, LocalDate> getVbs() {
+		return vbs;
+	}
+
+	public void setVbs(Map<EstadoComisionServicioEnum, LocalDate> vbs) {
+		this.vbs = vbs;
+	}
+	
+	public LocalDate getInicio() {
+		return inicio;
+	}
+
+	public void setInicio(LocalDate inicio) {
+		this.inicio = inicio;
+	}
+
+	public LocalDate getFin() {
+		return fin;
+	}
+
+	public void setFin(LocalDate fin) {
+		this.fin = fin;
+	}
 }
--- src/main/java/ucm/fdi/tfg/viajes/web/ComisionServicioController.java
+++ src/main/java/ucm/fdi/tfg/viajes/web/ComisionServicioController.java
@@ -1,10 +1,23 @@
 package ucm.fdi.tfg.viajes.web;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import ucm.fdi.tfg.proyecto.business.boundary.ProyectosManager;
+import ucm.fdi.tfg.proyecto.business.entity.Proyecto;
 import ucm.fdi.tfg.users.business.boundary.UserManager;
+import ucm.fdi.tfg.users.business.entity.Investigador;
+import ucm.fdi.tfg.users.business.entity.User;
+import ucm.fdi.tfg.viajes.business.entity.ComisionServicio;
+import ucm.fdi.tfg.viajes.business.entity.PermisoAusencia;
 
 @Controller
 public class ComisionServicioController {
@@ -19,5 +32,34 @@ public class ComisionServicioController {
 		this.users =users;
 	}
 	
+	@RequestMapping(value = "/proyectos/{idProyecto}/altaComisionServicio", method = RequestMethod.GET)
+	public ModelAndView ComisionServicioform(@PathVariable(value="idProyecto") Long idProyecto) {
+	Map<String, Object> model = new HashMap<String, Object>();
+		
+		//Cogemos el proyecto  que vamos a pintar en el Pago
+		Proyecto proyecto = proyectosManager.findProyecto(idProyecto);
+		
+		User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		
+		Investigador interesado = users.findInvestigador(user.getId());
+		
+		ComisionServicio comisionServicio = new ComisionServicio(proyecto,interesado);		
+		
+		model.put("modoTitulo", "Alta");
+		model.put("modo", "altaPermisoAusenciaViaje");	
+		
+		model.put("comisionServicio", comisionServicio);
+		
+		//Para el nombre en la cabecera
+		model.put("usuario", SecurityContextHolder.getContext().getAuthentication().getName());
+		
+		model.put("user", user);
+		
+		//model.put("interesado", interesado);
+
+		ModelAndView view = new ModelAndView("viajes/comisionServicios", model);
+		
+		return view;
+	}
 	
 }
