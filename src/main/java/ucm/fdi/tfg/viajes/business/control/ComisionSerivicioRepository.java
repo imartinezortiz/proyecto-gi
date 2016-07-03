f93443a2b2b865b9feed25f18ddfc9acd22e676b
David Nieto <david.nietocobo@gmail.com>
2016-07-03 18:27:16 +0200

    Creado flujo para buscar Comisiones Servicio segun el estado en el que esté

--- src/main/java/ucm/fdi/tfg/viajes/business/control/ComisionSerivicioRepository.java
+++ src/main/java/ucm/fdi/tfg/viajes/business/control/ComisionSerivicioRepository.java
@@ -1,11 +1,17 @@
 package ucm.fdi.tfg.viajes.business.control;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import ucm.fdi.tfg.viajes.business.entity.ComisionServicio;
+import ucm.fdi.tfg.viajes.business.entity.EstadoComisionServicioEnum;
 
 @Repository
 public interface ComisionSerivicioRepository extends JpaRepository<ComisionServicio, Long>{
+	
+	List<ComisionServicio> findByestado(EstadoComisionServicioEnum estado);
+
 
 }
